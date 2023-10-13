549a1e59a5c536a03f01b9a43e000ac79822982b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-13 18:29:07 -0300

    faet: improved performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), webfontDownload()],
 })
